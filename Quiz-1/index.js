4ad8de20e5dbba21fe7cfcffb16795ac5591b697
risangdevs <risangdevs@gmail.com>
2021-08-21 13:31:24 +0700

    Function Penghasil Tanggal Hari Esok

--- Quiz-1/index.js
+++ Quiz-1/index.js
@@ -2,25 +2,29 @@
 var tanggal=29
 var bulan=2
 var tahun=2020
-function next_date(){
-    if(tahun==2020){
-        if(bulan==2){
-            if(tanggal==29){
-                function feb(tanggal){
-                return tanggal-=28
-                }
-            }
-            else{
-                function febx(tanggal){
-                    return tanggal+=1
-                }
-            }
-        }
+if(tahun==2020 && bulan == 2 && tanggal== 29)
+{function next_date(tanggal,bulan,tahun){
+    tanggal=tanggal-=28
+    bulan=bulan+=1   
+    tahun=tahun
     }
 }
-console.log(tanggal,bulan,tahun)
-console.log(next_date)
+else if(tahun==2021 && bulan == 2 && tanggal ==28)
+{function next_date(tanggal,bulan,tahun){
+    tanggal=tanggal-=27
+    bulan=bulan+=1   
+    tahun=tahun
+    }
+}
 
+else if(tahun==2020 && bulan==12 && tanggal ==31)
+{function next_date(tanggal,bulan,tahun){
+    tanggal=tanggal-30
+    bulan=bulan-11   
+    tahun=tahun+1
+    }
+}
+console.log(next_date)
 //quiz 1 soal 2
 var kalimat_1 = "Halo nama saya Muhammad Iqbal Mubarok"
 var kalimat_2 = "Saya Iqbal"
