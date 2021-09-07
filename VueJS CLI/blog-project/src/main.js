be83307b2e4f01143f19d8166aacb5dfdee7917e
risangdevs <risangdevs@gmail.com>
2021-09-07 18:27:17 +0700

    Menambahkan Blog Project
    Blog project menggunakan Vue CLI, Vuetify, AXIOS API

---
+++ VueJS CLI/blog-project/src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import vuetify from './plugins/vuetify'
+import axios from './plugins/axios'
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  vuetify,
+  axios,
+  render: h => h(App)
+}).$mount('#app')
+
+// new Vue({
+//   el :'#app',
+//   components: { App },
+//   template : '<App />'
+// })
